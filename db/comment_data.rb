759116f53c772cc145efae4855e19ddb7341cc68
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-07-29 15:54:33 -0400

    Setup rails models, routes, and controllers - still have work to do on views

---
+++ db/comment_data.rb
@@ -0,0 +1,25 @@
+# comment_demo_1 = Comment.create({
+#   post_id: post_demo_1.id,
+#   body: "Demo 1's comment right here."
+#   })
+#
+# comment_demo_2 = Comment.create({
+#   post_id: post_demo_2.id,
+#   body: "Demo 2's better comment."
+#   })
+#
+# comment_demo_3 = Comment.create({
+#   post_id: post_demo_3.id,
+#   body: "Demo 3's really awesome comment"
+#   })
+#
+# comment_demo_4 = Comment.create({
+#   post_id: post_demo_4.id,
+#   body: "Demo 4 has great comments"
+#   })
+#
+# comment_demo_5 = Comment.create({
+#   post_id: post_demo_5.id,
+#   body: "Those demo 5 comments. Man oh man."
+#   })
+#
