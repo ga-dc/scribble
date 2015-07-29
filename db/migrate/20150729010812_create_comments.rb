9aa6e569a038d5236db499213bb3298e558cc585
Ryan Kahle <jrkahle@hotmail.com>
2015-07-28 21:14:00 -0400

    Added models and migrations for Posts and Comments

---
+++ db/migrate/20150729010812_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :author
+      t.text :comment
+      t.references :post, index: true, foreign_key: true
+      t.datetime :created_at
+    end
+  end
+end
