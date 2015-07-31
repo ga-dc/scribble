39a466e6fba13da46e873b11af4508fc0bbe66c8
Ryan Kahle <jrkahle@hotmail.com>
2015-07-30 22:58:16 -0400

    Need to add user id to posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ root to: "posts#index"
 resources :posts do
   resources :comments
 end
+get '/sign_in', to: 'users#sign_in'
+post '/sign_in', to: 'users#sign_in!'
+get '/sign_up', to: 'users#sign_up'
+post '/sign_up', to: 'users#sign_up!'
+get '/sign_out', to: 'users#sign_out'
 # get "" => "posts#index"
 #
 # get "posts" => "artists#index"
