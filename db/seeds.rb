4d76fb30c5200c058a9fd4acaa6119225082264b
Thomas Solow <thomassolow@gmail.com>
2015-11-09 18:07:29 -0500

    have migrations mapped out, not migrated yet.  have some seeds for posts only.  have routes for posts.  have controller for post templated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Comment.destroy_all
+Post.destroy_all
+
+posts = Post.create([
+  {author: "Jim", text: "This is a long blog post."},
+  {author: "Christine", text: "This is also a long blog post."},
+  {author: "Ted", text: "1. The Industrial Revolution and its consequences have been a disaster for the human race. They have greatly increased the life-expectancy of those of us who live in “advanced” countries, but they have destabilized society, have made life unfulfilling, have subjected human beings to indignities, have led to widespread psychological suffering (in the Third World to physical suffering as well) and have inflicted severe damage on the natural world. The continued development of technology will worsen the situation. It will certainly subject human beings to greater indignities and inflict greater damage on the natural world, it will probably lead to greater social disruption and psychological suffering, and it may lead to increased physical suffering even in “advanced” countries."}
+  ])
