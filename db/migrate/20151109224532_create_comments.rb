c66260b6dcd291488471c0709b5007102988c510
Thomas Solow <thomassolow@gmail.com>
2015-11-09 17:51:18 -0500

    added some cols in the migration files, not migrated yet

--- db/migrate/20151109224532_create_comments.rb
+++ db/migrate/20151109224532_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :author
+      t.string :text
+      t.datetime :posted_at
     end
   end
 end
--- db/migrate/20151109224536_create_posts.rb
+++ db/migrate/20151109224536_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string :author
+      t.string :text
+      t.datetime :posted_at
     end
   end
 end
