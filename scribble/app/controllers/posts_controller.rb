7b3a8a93c33bbaf48c45543f065cf308adc313d4
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-11-09 23:56:37 -0500

    adds all post-related except delete

---
+++ scribble/app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  # actions will go here
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @post = Post.create!(post_params)
+    redirect_to "/posts"
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update!(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def delete
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/posts"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :post)
+  end
+
+end
