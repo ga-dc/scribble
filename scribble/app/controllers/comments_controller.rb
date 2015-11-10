25f3725419644c404806641ad8025026824d3e51
Tyler <tylerscottcrosse@gmail.com>
2015-11-10 18:01:00 -0500

    working on editing comments

--- scribble/app/controllers/comments_controller.rb
+++ scribble/app/controllers/comments_controller.rb
@@ -6,6 +6,17 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @post.comment.update(post_params)
+
+    redirect_to post_path(@post)
+  end
+
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
