92511193fba119ff09d4f0ec3912daa124b89b82
ANDRE SHONUBI <andre@codedre.com>
2015-11-10 18:04:28 -0500

    4 out of 7 CRUD actions up for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,23 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.find(params[:id])
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create!(post_params)
+
+    redirect_to @post
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :post_image, :content, :author)
   end
 end
