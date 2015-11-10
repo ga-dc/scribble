f99d5403a9e4da44bf2d06104557cce974a86df5
Matthew Murgia <matthew.murgia@gmail.com>
2015-11-09 20:11:19 -0500

    create and migrate post class

---
+++ db/migrate/20151110004305_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :author
+      t.string :post
+      t.datetime :date
+    end
+  end
+end
