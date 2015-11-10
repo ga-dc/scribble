1246621e9cfca3fcc1657e6b5d29613f7bdd803d
Peter Zhang <pzhang87@gmail.com>
2015-11-10 14:51:08 -0500

    implement routes helper method to clean up routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  get    '/posts'           => 'posts#index'
-  get    '/posts/new'       => 'posts#new'
-  get    '/posts/:id'       => 'posts#show'
-  post   '/posts'           => 'posts#create'
-  get    '/posts/:id/edit'  => 'posts#edit'
-  put    '/posts/:id'       => 'posts#update'
-  delete '/posts/:id'       => 'posts#destroy'
-
-  get    '/comments'           => 'comments#index'
-  get    '/comments/new'       => 'comments#new'
-  get    '/comments/:id'       => 'comments#show'
-  post   '/comments'           => 'comments#create'
-  get    '/comments/:id/edit'  => 'comments#edit'
-  put    '/comments/:id'       => 'comments#update'
-  delete '/comments/:id'       => 'comments#destroy'
+  resources :posts
+  resources :comments
 end
