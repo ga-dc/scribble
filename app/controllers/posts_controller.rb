f62dcc47df370268f45a7cd8cb3461297a19a404
Chase Gruber <chasegruber@gmail.com>
2015-11-09 20:03:56 -0500

    block out posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  # posts#index
+  def index
+    @posts = Post.all
+  end
+  # posts#show
+  def show
+    @post = Post.find(params[:id])
+  end
+  # posts#create
+  def create
+    @post = Post.create(post_params)
+  end
+  # posts#new
+  def new
+    @post = Post.new()
+  end
+  # posts#edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+  # posts#update
+  def update
+    @post = Post.find(params[:id])
+  end
+  # posts#destroy
+  def destroy
+    Post.delete([params[:id]])
+  end
+
+end
