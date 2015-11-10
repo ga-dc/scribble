fd084d2a32eecd454f735952aaa8563646b525ef
Tyler <tylerscottcrosse@gmail.com>
2015-11-10 12:12:13 -0500

    added destroy to comments

--- scribble/app/controllers/comments_controller.rb
+++ scribble/app/controllers/comments_controller.rb
@@ -2,6 +2,15 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
     redirect_to post_path(@post)
   end
 
