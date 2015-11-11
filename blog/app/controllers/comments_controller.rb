d9925b9de0fae92754628233a7ebd444d4a95a9b
alea qawiyy <aq1984@gmail.com>
2015-11-10 20:29:28 -0500

    post works

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,3 +1,33 @@
 class CommentsController < ApplicationController
+  #index
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.create(post_params)
+    redirect_to post_path(@comment)
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 
 end
--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,3 +1,34 @@
 class PostsController < ApplicationController
+  #index
+  def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+
+    @post = Post.create!(post_params)
+    redirect_to posts_path(@post)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
