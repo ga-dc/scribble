581f2d891a8c7d4308284ffa2263ec6feab0e885
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-11-11 01:39:46 -0500

    ???

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  validates :subject, presence: true,
-                    length: { minimum: 5 }
+  # post_id :true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'comments/index'
+  # get 'comments/index'
 
   get 'posts/index'
 
