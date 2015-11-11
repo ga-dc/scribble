8d25e5769db8e7dfff1ee7ef71d6b5d4266bf33e
Samir Mehta <sammehta88@gmail.com>
2015-11-11 06:37:56 -0500

    Add views for comments

--- scribble/app/controllers/comments_controller.rb
+++ scribble/app/controllers/comments_controller.rb
@@ -1,3 +1,30 @@
 class CommentsController < ApplicationController
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comments_params)
+
+    redirect_to post_comment_path(@post, @comment)
+  end
+
+
+  private
+  def comments_params
+    params.require(:comment).permit(:text)
+  end
 end
