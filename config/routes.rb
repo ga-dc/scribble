a24fdd7fe0a0f0b60b3e30cc816876387c47e6d8
James Moon <mooniker@gmail.com>
2015-11-10 21:08:02 -0500

    Add view for Posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'comments/show'
 
-  get 'posts/show'
-
-  get 'comments/index'
-
-  get 'posts/index'
+  resources :posts do
+    resources :comments
+  end
+  
+  # get 'comments/show'
+  #
+  # get 'posts/show'
+  #
+  # get 'comments/index'
+  #
+  # get 'posts/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
