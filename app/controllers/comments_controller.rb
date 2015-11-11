769494c15773eda0a4dc69f8eb915ddd640f8379
Thomas Solow <thomassolow@gmail.com>
2015-11-10 21:10:04 -0500

    All CRUD working, displaying edit time on comments not working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,22 +5,33 @@ class CommentsController < ApplicationController
   end
   def show
     @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:comment_id])
+    @comment = Comment.find(params[:id])
   end
   def new
     @post = Post.find(params[:post_id])
+    @comment = Comment.new
   end
   def create
     @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comments_params)
+    redirect_to post_path(@post)
   end
   def destroy
     @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
   end
   def edit
     @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+
   end
   def update
     @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update!(comments_params)
+    redirect_to post_path(@post)
   end
   private
   def comments_params
