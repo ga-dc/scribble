e55bac341329e0622d3c2bc10cad85446edffdbb
Samir Mehta <sammehta88@gmail.com>
2015-11-11 06:48:25 -0500

    Add remaining RESTful routes to comments;
    git status
    ;

--- scribble/app/controllers/comments_controller.rb
+++ scribble/app/controllers/comments_controller.rb
@@ -22,9 +22,25 @@ class CommentsController < ApplicationController
     redirect_to post_comment_path(@post, @comment)
   end
 
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comments_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
 
   private
   def comments_params
     params.require(:comment).permit(:text)
   end
 end
+
+
+# question - is @comment.post faster than Post.find(params[:artist_id])?
+# which is better practice? Is consistancy more important?
