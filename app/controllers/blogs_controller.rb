cffc55aed2028c12a9b8068a96086b9ff9082ee6
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-11-10 20:59:41 -0500

    can edit and delete blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -11,10 +11,26 @@ class BlogsController < ApplicationController
 		@blog = Blog.find(params[:id])
 	end
 
+	def edit
+		@blog = Blog.find(params[:id])
+	end
+
 	def create
     	@blog = Blog.create!(blog_params)
-    	redirect_to blog_index_path
+    	redirect_to blog_path(@blog)
 	end
+
+	def update
+   	 	@blog = Blog.find(params[:id])
+   		@blog.update(blog_params)
+		redirect_to blog_path(@blog)
+	end
+
+	def destroy
+	    @blog = Blog.find(params[:id])
+	    @blog.destroy
+	    redirect_to blogs_path
+  	end
 
 	private
  	def blog_params
