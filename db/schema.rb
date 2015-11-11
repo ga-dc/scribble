0f2bd22313489b651a5eb5b7b4fba54b0518b0f2
ckib16 <ckib16@gmail.com>
2015-11-10 22:27:29 -0500

    controllers realtionships and erd built

--- db/schema.rb
+++ db/schema.rb
@@ -17,13 +17,19 @@ ActiveRecord::Schema.define(version: 20151111030421) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
