23d92b4b5264a12e2a630f18caaf6beb75010c7b
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-11-11 15:03:27 -0500

    blog comments have U & D actions, need to refine some redirects

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,19 +11,33 @@ class CommentsController < ApplicationController
 	def create
 		@blog = Blog.find(params[:blog_id])
 		@comment = Comment.create!(comment_params.merge(blog: @blog))
-		# @comment = @blog.comments.create(comment_params)
-		redirect_to blog_comment_path(@blog, @comment.id)
+		redirect_to blog_comment_path(@blog, @comment)
 	end
 
 	def show
 		@blog = Blog.find(params[:blog_id])
-		# @comment_blog_id = params[:blog_id]
 		@comment = Comment.find_by(blog_id = @comment_blog_id)
 	end
 
 	def edit
 		@blog = Blog.find(params[:blog_id])
+		@comment = Comment.find(params[:id])
 	end
+
+ 	def update
+	    @comment = Comment.find(params[:id])
+	    @blog = Blog.find(params[:blog_id])
+	    @comment.update(comment_params.merge(blog: @blog))
+		redirect_to blog_path(@blog)
+		# need to make this direct to blog
+ 	end
+
+ 	def destroy
+	    @blog = Blog.find(params[:blog_id])
+	    @comment = Comment.find(params[:id])
+	    @comment.destroy
+	    redirect_to blog_path(@blog)
+  	end
 
 	private
 	def comment_params
