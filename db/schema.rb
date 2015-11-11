2334ac3d039a80a570afcc148e6060f0a7032067
Teddy Kentor <tkentor@gmail.com>
2015-11-11 14:09:48 -0500

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,3 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20151111132500) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.string "user"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
-  end
-
-end
