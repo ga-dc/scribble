241e60aed0ebdce60f3853f4fcd7ef1b3d458c7e
Starr Z. Chen <chenstarr@gmail.com>
2015-11-11 00:13:11 -0500

    Almost forgot to migrate whoops

--- scribble/db/migrate/20151110023158_create_posts.rb
+++ scribble/db/migrate/20151110023158_create_posts.rb
@@ -1,7 +1,6 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.integer :id
       t.string :title
       t.string :content
       t.datetime :created_at
--- scribble/db/migrate/20151110031525_create_comments.rb
+++ scribble/db/migrate/20151110031525_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer :id
       t.string :username
       t.string :content
       t.datetime :created_at
