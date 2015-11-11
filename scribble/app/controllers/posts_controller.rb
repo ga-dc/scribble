042e8194ab0897276766136573e9f036879ecf5c
Samir Mehta <sammehta88@gmail.com>
2015-11-11 06:15:17 -0500

    Add delete action

--- scribble/app/controllers/posts_controller.rb
+++ scribble/app/controllers/posts_controller.rb
@@ -15,4 +15,26 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+  def create
+    @post = Post.create!(posts_params)
+    redirect_to @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(posts_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def posts_params
+    params.require(:post).permit(:title, :text)
+  end
 end
