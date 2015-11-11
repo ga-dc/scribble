f79c03d75bd96e7fe87421e30eee2e4a6e794e4c
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-11-11 14:16:16 -0500

    fixes path helpers, adds initial user model and authentication

--- scribble/app/controllers/posts_controller.rb
+++ scribble/app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create!(post_params)
-    redirect_to "/posts"
+    redirect_to posts_path
   end
 
   def edit
@@ -24,13 +24,13 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update!(post_params)
-    redirect_to "/posts/#{@post.id}"
+    redirect_to post_path(@post)
   end
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to "/posts"
+    redirect_to posts_path
   end
 
   private
