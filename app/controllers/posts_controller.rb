9200fc0a1363a749b556af4ff085b5f4135b5911
Chase Gruber <chasegruber@gmail.com>
2015-11-10 20:37:02 -0500

    helper paths in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   # posts#create
   def create
     @post = Post.create!(post_params)
-    redirect_to "/posts/#{@post.id}"
+    redirect_to post_path(@post)
   end
   # posts#new
   def new
@@ -24,12 +24,12 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to "/posts/#{@post.id}"
+    redirect_to post_path(@post)
   end
   # posts#destroy
   def destroy
     Post.delete([params[:id]])
-    redirect_to "/posts"
+    redirect_to posts_path
   end
 
   private
