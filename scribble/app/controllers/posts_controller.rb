6a0d849e2d2109b4df1e3da933793445f26ed2ca
Samir Mehta <sammehta88@gmail.com>
2015-11-11 05:34:43 -0500

    Add new and index actions to posts controller and create corresponding views

---
+++ scribble/app/controllers/posts_controller.rb
@@ -0,0 +1,10 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+# before adding the new action, when I clicked on the the + button on
+# the index page, it took me to a blank page, no error thrown.
+# I was expecting a undefined method error
+  def new
+  end
+end
