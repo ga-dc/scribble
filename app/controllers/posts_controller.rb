1d55a030cc894198c74f41dd8cb24e2384a9d92b
ckib16 <ckib16@gmail.com>
2015-11-12 17:48:57 -0500

    fully working Post controller & views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,11 +41,12 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Use callbacks for common actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
+    #Whitelist params (strong params)
     def post_params
       params.require(:post).permit(:title, :body)
     end
