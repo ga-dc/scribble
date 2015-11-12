bd771cc331403671d8fb278d16624ad2f0dbdf44
Chase Gruber <chasegruber@gmail.com>
2015-11-12 17:02:57 -0500

    some more css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,6 +30,11 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def show
+    @post = Post.find(params[:post_id])
+    redirect_to post_path(@post)
+  end
+
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
