6de36a381a6e15938f1ad46c4f4ebd4e11885fd5
Dumitru Gorgan <dgorgan3@gmail.com>
2015-11-12 16:47:49 -0500

    can comment on posts. mostly working but still need to add functionality to edit and destroy comments. also changed the layout on the posts index to be more blog like

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,32 @@
 class CommentsController < ApplicationController
 
   def index
-  end
-
-  def create
+    @post = Post.all
   end
 
   def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(comment_params)
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
 
   def edit
   end
 
   def show
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
   end
 
   def update
@@ -22,6 +36,10 @@ class CommentsController < ApplicationController
   end
 
 
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 
 
 
