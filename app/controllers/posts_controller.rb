ba812b56056454a24512b9d6db9278272b66c75e
ckib16 <ckib16@gmail.com>
2015-11-12 17:01:33 -0500

    Trying to recover
    Having a git and PG error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -11,27 +12,41 @@ class PostsController < ApplicationController
   def create
     @post = Post.create!(post_params)
       #assign result to @post just so can pass @post into redirect_to below to get the :id
-    redirect_to post_path(@post)
+    if @post.save
+      flash[:notice] = "Successfully created..."
+      redirect_to post_path(@post)
+    else
+
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-
   end
 
   def update
-
+    if @post.update(post_params)
+      flash[:notice] = "Successfully edited..."
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
