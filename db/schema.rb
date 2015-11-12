b61783c4bd322ba84be5d81d5376221f238072ad
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-11-12 17:42:37 -0500

    begin bonus attempt

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112190322) do
+ActiveRecord::Schema.define(version: 20151112223430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "comment"
@@ -33,6 +39,16 @@ ActiveRecord::Schema.define(version: 20151112190322) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
+
+  create_table "tags", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "tags", ["category_id"], name: "index_tags_on_category_id", using: :btree
+  add_index "tags", ["post_id"], name: "index_tags_on_post_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -53,4 +69,6 @@ ActiveRecord::Schema.define(version: 20151112190322) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "tags", "categories"
+  add_foreign_key "tags", "posts"
 end
