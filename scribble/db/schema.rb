7461ab27ab8c214f695f9bee77eaed1cbfd32507
Kelly Austin <kaustly@gmail.com>
2015-11-12 20:10:38 -0500

    functional_comments

--- scribble/db/schema.rb
+++ scribble/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112232050) do
+ActiveRecord::Schema.define(version: 20151113004857) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
