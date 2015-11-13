d151086c6a674ac2da207e3945cef92ee0c36a14
James Moon <mooniker@gmail.com>
2015-11-12 20:07:44 -0500

    Second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
 end
