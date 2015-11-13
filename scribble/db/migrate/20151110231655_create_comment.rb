ab8ba5e2104bb3f68bbd5a94be518231c4ba5403
Qadriyyyah Thomas <qmadinah@gmail.com>
2015-11-12 20:19:15 -0500

    create authentication for post and comments

--- scribble/db/migrate/20151110231655_create_comment.rb
+++ scribble/db/migrate/20151110231655_create_comment.rb
@@ -1,6 +1,11 @@
 class CreateComment < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+      t.references :post, index: true, foreign_key: true
+
+      t.timestamps null: false
     end
   end
 end
--- scribble/db/schema.rb
+++ scribble/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110231655) do
+ActiveRecord::Schema.define(version: 20151112220902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -26,4 +33,10 @@ ActiveRecord::Schema.define(version: 20151110231655) do
     t.datetime "updated_at"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
+  add_foreign_key "comments", "posts"
 end
