41e0944ef5f6e4aba70777cdca9fab0680503830
Rachel Porter <rachelandrewsporter@gmail.com>
2015-11-12 21:48:13 -0500

    fixes CRUD routes for posts and comments

--- scribble/app/controllers/comments_controller.rb
+++ scribble/app/controllers/comments_controller.rb
@@ -1,57 +1,67 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+
   # index
-    def index
-      @post = Post.find(params[:post_id])
-      @comments = Comment.all
-    end
+  def index
+    # @post = Post.find(params[:post_id])
+    # @user = current_user
+    # @comments = Comment.all
+  end
 
-    # new
-    def new
-      @post = Post.find(params[:post_id])
-      @comment = Comment.new
-    end
+  # new
+  def new
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
 
-    # create
-    def create
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.create(comment_params)
-      redirect_to post_path(@post)
-    end
+  # create
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    redirect_to post_path(@post)
+  end
 
-    #show
-    def show
-      @post = Post.find(params[:post_id])
-      @comment = Comment.find(params[:id])
-    end
+  #show
+  def show
 
-    # edit
-    def edit
-      @post = Post.find(params[:post_id])
-      @comment = Comment.find(params[:id])
-    end
+  end
 
-    # update
-    def update
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.find(params[:id])
-      @comment.update(comment_params)
+  # edit
+  def edit
 
-      redirect_to post_path(@post)
-    end
+  end
 
-    # destroy
-    def destroy
-      @post = Post.find(params[:post_id])
-      @comment = Comment.find(params[:id])
-      @comment.destroy
+  # update
+  def update
+    @comment = @post.comments.find(params[:id])
+    @comment.update(comment_params)
 
-      redirect_to post_path(@post)
-    end
+    redirect_to post_path(@post)
+  end
 
-    private
-    def comment_params
-      params.require(:comment).permit(:username, :comment, :username)
-    end
+  # destroy
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
 
+    redirect_to post_path(@post)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id, :comment)
+  end
+
+  def set_comment
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @commenter = current_user.email
+  end
 
 end
--- scribble/app/controllers/posts_controller.rb
+++ scribble/app/controllers/posts_controller.rb
@@ -25,12 +25,11 @@ def create
 end
 #show
 def show
-  @post = Post.find(params[:id])
-  @comments = @post.comments
+
 end
 
 def edit
-  @post = Post.find(params[:id])
+
 end
 
 def update
@@ -50,7 +49,13 @@ end
 
 private
   def post_params
-    params.require(:user).permit(:title, :message, :image_url)
+    params.require(:post).permit(:title, :message, :image_url)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+    @user = current_user
+    @comments = @post.comments
   end
 
 end
