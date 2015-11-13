50db6c236ad7524487adf7c8dc3646874bedfb64
Kelly Austin <kaustly@gmail.com>
2015-11-13 08:27:04 -0500

    sort_posts

--- scribble/app/controllers/posts_controller.rb
+++ scribble/app/controllers/posts_controller.rb
@@ -1,15 +1,25 @@
 class PostsController < ApplicationController
+
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # this action will set the `sort_by` property
+  def sort
+    session[:sort_by] = params[:sort_by]
+    redirect_to "/"
+  end
+
+  # in our index, sort by the session value
 
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(session[:sort_by])
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
+    session[:last_viewed_post_id]= @post.id
   end
 
   # GET /posts/new
@@ -59,6 +69,13 @@ class PostsController < ApplicationController
       format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def delete_session
+  session.delete(:last_viewed_post)
+  # or reset_session
+  # which resets the entire session hash
+  redirect_to posts_path
   end
 
   private
