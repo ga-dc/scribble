428d80db55cd3a0714b58d9efa90f06b0bfd9167
Alexandra <akscreven@gmail.com>
2015-11-13 13:58:46 -0500

    everything good, need to add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20151110234234) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
     t.string   "photo_url"
+    t.string   "title"
     t.text     "text"
-    t.datetime "created_at"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
@@ -29,6 +32,7 @@ ActiveRecord::Schema.define(version: 20151110234234) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
+  add_foreign_key "comments", "articles"
 end
