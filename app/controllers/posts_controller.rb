c5ad1841f0e8aac2b9e1e0f3fed3668bfead931e
ANDRE SHONUBI <andre@codedre.com>
2015-11-12 19:35:50 -0500

    made a lot of changes should have save early

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -9,27 +10,23 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create!(post_params)
+    @post = current_user.posts.create!(post_params, author: current_user.email)
 
     redirect_to @post
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to root_path
   end
@@ -37,5 +34,9 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:title, :post_image, :content, :author)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])#code
   end
 end
