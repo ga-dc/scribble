7b5ba9d2f50e07e687884e76c9b1f118bd797a89
Thomas Clark <tclark@soteriagiving.com>
2015-11-15 18:11:53 -0500

    add article controller and index.erb

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+  @article = Article.find(params[:id])
+end
+
   def create
   @article = Article.new(article_params)
 
@@ -29,6 +33,13 @@ def update
   else
     render 'edit'
   end
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
 end
 
   private
