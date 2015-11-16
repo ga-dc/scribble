9843a9a1765f6a3cc1422c2ed917dfeee7451f55
Thomas Clark <tclark@soteriagiving.com>
2015-11-15 19:01:03 -0500

    completed app with tutorial, will add css later

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,46 +1,49 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
-      @articles = Article.all
-    end
+    @articles = Article.all
+  end
 
   def show
-      @article = Article.find(params[:id])
-    end
+    @article = Article.find(params[:id])
+  end
 
-    def new
+  def new
     @article = Article.new
   end
 
   def edit
-  @article = Article.find(params[:id])
-end
+    @article = Article.find(params[:id])
+  end
 
   def create
-  @article = Article.new(article_params)
+    @article = Article.new(article_params)
 
-  if @article.save
-    redirect_to @article
-  else
-    render 'new'
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
-end
 
-def update
-  @article = Article.find(params[:id])
+  def update
+    @article = Article.find(params[:id])
 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
-end
 
-def destroy
-  @article = Article.find(params[:id])
-  @article.destroy
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-  redirect_to articles_path
-end
+    redirect_to articles_path
+  end
 
   private
   def article_params
