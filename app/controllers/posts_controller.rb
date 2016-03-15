25342a2b9ae78f30e6c2c807603aec73aadfdd85
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-15 13:01:15 -0400

    feature to add posts created and working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create!(post_params)
+
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def detroy
+
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,
+    :user,:content)
+  end
 end
