e5297c061207b25fe5f4058f2d22f1ea5ff6ef42
Melissa Miller <lis.d.miller@gmail.com>
2016-03-15 12:50:11 -0400

    Edits schema and and controller for posts
    Adds timestamp to schema. Creates index route.

--- scribble/db/schema.rb
+++ scribble/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314180317) do
+ActiveRecord::Schema.define(version: 20160315123731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(version: 20160314180317) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
--- scribble/db/seeds.rb
+++ scribble/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.destroy_all
+Comment.destroy_all
+
+Post.create(title: 'Sample title', content: "Sample post text. Blah blah blah. I write things!")
