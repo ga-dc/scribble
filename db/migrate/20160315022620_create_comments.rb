05137a5bf89d04d25fed40d472a56e05f44beef4
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-14 22:31:06 -0400

    comments table migrated

--- db/migrate/20160315022620_create_comments.rb
+++ db/migrate/20160315022620_create_comments.rb
@@ -1,6 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.string :commentor
+      t.timestamp
+      t.boolean :reaction
+      t.integer :post_id
     end
   end
 end
