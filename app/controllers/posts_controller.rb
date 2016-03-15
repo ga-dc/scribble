2400e5247b25db1221c2ccc8b16a3a506c75501f
Charles <charlesthwang@gmail.com>
2016-03-15 19:39:54 -0400

    able to create, edit, delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,15 +17,24 @@ class PostsController < ApplicationController
     puts params
     puts "New Post Created"
     @post = Post.create!(post_params)
-    redirect_to "/posts"
+    redirect_to posts_path(@post)
   end
 
   def edit
     @post = Post.find(params[:id])
   end
-  
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path(@post)
+
+  end
+
   def destroy
-    @post = Post.delete
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path(@post)
   end
 
   private
