bc0b5933d385275afd194ca43695908bb5fe6601
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-15 01:07:21 -0400

    added views modified controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,14 @@ class CommentsController < ApplicationController
   def index
     @comments = Comment.all
   end
-end
+
+  def new
+    @comment = Comment.new
+  end
+
+private
+  def comment_params
+  params.require(:comment).permit(:content)
+  end
+
+end 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,14 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+private
+  def post_params
+  params.require(:post).permit(:content)
+  end
 end
