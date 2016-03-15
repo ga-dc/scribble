c706f772b485647b211696fd0ed8e2a74fb9c0f2
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-15 18:03:54 -0400

    added edit and destroy to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,13 +18,25 @@ class PostsController < ApplicationController
     redirect_to "/posts/#{@post.id}"
   end
 
-  def detroy
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
 
+    redirect_to posts_url
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.update(params[:id],post_params)
+    redirect_to post_url(@post)
   end
 
   private
   def post_params
     params.require(:post).permit(:title,
-    :user,:content)
+    :user,:post_id, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root to: "posts#index"
+  # resources :comments
   # get "posts" => "posts#index"
   # get "posts/:id" => "posts#show"
 end
