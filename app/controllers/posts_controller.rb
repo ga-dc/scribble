37d876f8d8eb38baaadf147c9b35837271c1d64a
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-15 18:04:00 -0400

    4th commit: controller and view updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,38 @@
 class PostsController < ApplicationController
+  def index
+      @posts = Post.all
+    end
 
+    def new
+      @post = Post.new
+    end
+
+    def create
+      @post = Post.create!(post_params)
+      redirect_to posts_path
+    end
+
+    def show
+      @post = Post.find(params[:id])
+    end
+
+    def edit
+      @post = Post.find(params[:id])
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+
+    end
+
+    private
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
+
+  end
 end
--- db/migrate/20151111000815_create_post.rb
+++ db/migrate/20151111000815_create_post.rb
@@ -1,6 +1,7 @@
 class CreatePost < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.int     :time
       t.string  :title
       t.text    :description
     end
