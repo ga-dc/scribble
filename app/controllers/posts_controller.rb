f62f473a2e84a623a0ddfa82e22544ef0cd350f2
Anissa Jones <anissajones10@gmail.com>
2016-03-16 19:03:37 -0400

    Some how I had two Posts controllers. Closed atom and reopened. changes were not committed.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,28 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
+
     def new
         @post = Post.new
     end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
     def create
         @post = Post.create! post_params
+        redirect_to post_path(@post)
+    end
+
+    def update
+        @post = Post.edit! post_params
         redirect_to "/posts"
     end
+
+    def destroy
+    end
+
 
     private
     def post_params
