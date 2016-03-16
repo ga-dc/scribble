2ccf41af16f3eb24e14d3b421380d36648ff80ca
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-16 10:14:51 -0400

    added comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+
+  def new
+    @comment = Comment.new
+  end
+
+
+  def create
+    @comment = Comment.create(comment_params)
+  end
+
+
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+end
