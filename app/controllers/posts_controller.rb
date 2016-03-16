80fad557858d7ac5026bd6bfbdd988966cc6b2d6
Cam <camlatimer@gmail.com>
2016-03-16 14:00:51 -0400

    added posts_controller.rb and posts#index file

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+# customary in rails to put actions in order like:
+# index, show, new, edit, create, update, destroy
+  def index
+
+  end
+
+  def show
+
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+end
