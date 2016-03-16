6c3d4d880cbd3d825d9f27d9dd5644c7ae2163fe
Cam <camlatimer@gmail.com>
2016-03-16 16:49:23 -0400

    added comments area in posts/show.html.erb so you users can add a comment to a post and see all comments for a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,12 @@ class CommentsController < ApplicationController
   end
 
   def create
+    # creates a new comment, linked to a particular article
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
 
+    # calls posts#show action and sends user back to the article the comment was made on
+    redirect_to post_path(@post)
   end
 
   def update
@@ -27,6 +32,11 @@ class CommentsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :content)
   end
 
 end
