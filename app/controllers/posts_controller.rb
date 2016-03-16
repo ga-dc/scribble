d7a6a47ee877e7a1f94267983c458d323648874b
Bipin Neupane <icomp2@hotmail.com>
2016-03-15 20:39:33 -0400

    time boxed, trying to get new blog post to show up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,15 @@
 class PostsController < ApplicationController
-def create
-  # grab the user input for params
-  # create a new post from that input
-  # redirect back to the index
-  redirect_to :back
-end
-def index
-  @posts = Post.all
+  private
+  def create
+    @Posts = Post.create!(post: params[:post])
+    # grab the user input for params
+    # create a new post from that input
+    # redirect back to the index
+    redirect_to :back
+  end
+  def index
+    @posts = Post.all
+    render :index
 
-end
+  end
 end
