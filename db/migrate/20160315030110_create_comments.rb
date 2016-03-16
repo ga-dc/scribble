04cf028cbd5bf93ee4fe9d1315ca328a639a8551
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-15 20:57:20 -0400

    moved some code to application files from post index

--- db/migrate/20160315030110_create_comments.rb
+++ db/migrate/20160315030110_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :content
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
