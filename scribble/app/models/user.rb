d036f631f4e0bc91f362d2a408a3160e97232302
Patrick Moran <patrick_moran@mac.com>
2016-03-16 15:09:21 -0400

    ensured email uniqueness for users buy adding an index to email column of users

--- scribble/app/models/user.rb
+++ scribble/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+                    format: {with: VALID_EMAIL}, uniqueness: {case_sensitive: false}
 end
--- scribble/db/schema.rb
+++ scribble/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312032341) do
+ActiveRecord::Schema.define(version: 20160316190457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,5 +29,14 @@ ActiveRecord::Schema.define(version: 20160312032341) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
