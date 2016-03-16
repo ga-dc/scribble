5efdc92f7c3667fc99f48274c71c33035d6b6808
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-15 20:45:10 -0400

    Commit before leaving school

--- db/migrate/20160316000756_create_posts.rb
+++ db/migrate/20160316000756_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :name
+      t.string :title
       t.string :author
       t.string :author_email
-      t.string :post
+      t.string :post_content
       t.string :post_tag
       t.string :category
     end
