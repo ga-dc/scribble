86efe815159e0e7e15a0cde0801d258ad1010166
Ryan Krolick <rkrolick@gmail.com>
2016-03-16 18:26:05 -0400

    Basic post functionality added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.destroy_all
+Comment.destroy_all
+
+Post.create(title: "Mohamed", body: "Is the worst...")
+Post.create(title: "Pants", body: "Are somehow worse than Mohamed")
+Comment.create(comment_text: "Like barely though...", post_id: 2)
