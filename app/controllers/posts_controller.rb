958070136b877c92c2fb2234d7011cedbc73c99c
Anissa Jones <anissajones10@gmail.com>
2016-03-16 12:38:55 -0400

    recieveing error when trying to view a clicked on post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,29 +2,13 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
-
     def new
         @post = Post.new
     end
-
-    def show
-        puts params
-        @post = Post.find(params[:id])
-    end
-
     def create
         @post = Post.create! post_params
         redirect_to "/posts"
     end
-
-    def update
-        @post = Post.edit! post_params
-        redirect_to "/posts"
-    end
-
-    def destroy
-    end
-
 
     private
     def post_params
