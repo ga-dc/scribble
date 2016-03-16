c9cdaa5ad65d47eddf4b92f7e7415caec03d321c
Cam <camlatimer@gmail.com>
2016-03-16 16:26:54 -0400

    started comments_controller.rb and added functionality for adding to comments in posts/show.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,32 @@
 class CommentsController < ApplicationController
+  # customary in rails to put actions in order like:
+  # index, show, new, edit, create, update, destroy
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
