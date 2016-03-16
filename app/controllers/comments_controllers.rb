bfeb74b70b9c3f3d1d0a33bdbd4c674529d677c0
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-16 14:47:27 -0400

    Completing controllers.

--- app/controllers/comments_controllers.rb
+++ app/controllers/comments_controllers.rb
@@ -1,23 +1,26 @@
 class CommentsController < ApplicationController
 
   def index
-
+    @comments = Comments.all
   end
 
   def create
-
+    @comment = Comments.create(comment_params)
+    redirect_to "/comments/#{@artist.id}"
   end
 
   def show
-
+    @comment = Comment.find(params[:id])
   end
 
   def update
-
+    @comment =
+    @comment =
   end
 
   def delete
-
+    @comment =
+    @comment =
   end
 
 end
--- app/controllers/posts_controllers.rb
+++ app/controllers/posts_controllers.rb
@@ -1,23 +1,26 @@
 class PostsController < ApplicationController
 
-  def index
+    def index
+      @posts = Posts.all
+    end
+
+    def create
+      @post = Posts.create(comment_params)
+      redirect_to "/comments/#{@artist.id}"
+    end
+
+    def show
+      @post = Post.find(params[:id])
+    end
+
+    def update
+      @post = Post.find
+      @post = Post.
+    end
+
+    def delete
+      @post = Post.
+      @post = Post.
+    end
 
   end
-
-  def create
-
-  end
-
-  def show
-
-  end
-
-  def update
-
-  end
-
-  def delete
-
-  end
-
-end
