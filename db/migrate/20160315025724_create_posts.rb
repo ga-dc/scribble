df87bae547c8414adcc3ef09380d5b09b2de2157
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-17 14:03:45 -0400

    rolled back to previous, added form, working view

--- db/migrate/20160315025724_create_posts.rb
+++ db/migrate/20160315025724_create_posts.rb
@@ -1,12 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.id :id
       t.string :title
-      t.author :author
-      t.timestamp
+      t.string :author
+      t.timestamps
       t.text :post_content
-      t.tag :tag_id
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,14 +17,17 @@ ActiveRecord::Schema.define(version: 20160315035541) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "post_content"
   end
 
   create_table "users", force: :cascade do |t|
