4436c26524173ac0b053a835d9a3025be17b553c
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-17 18:37:19 -0400

    seed file complete with comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,9 @@ trip = Post.create(title: "Top 10 Places to Visit this Summer", author: "Alexa K
 fun = Post.create(title: "Top 5 Activities in D.C. this Spring", author: "El Presidente", photo_url: "")
 eat = Post.create(title: "Top DC Restaurants and Bars", author: "Hungry Sam", photo_url: "")
 history = Post.create(title: "7 Historical DC Facts you Don't Know!", author: "Uncle Sam", photo_url:"")
+
+happy = Comment.create(commentor: "Love-to-Travel", reaction: true, post_id: 1)
+bored = Comment.create(commentor: "Bored-at-Home", reaction: false, post_id: 3)
+hungry = Comment.create(commentor: "Hungry-for-More", reaction: true, post_id: 3)
+historical = Comment.create(commentor: "History Buff", reaction: false, post_id: 4)
+hot = Comment.create(commentor: "Some Like it Hot", reaction: true, post_id: 1)
