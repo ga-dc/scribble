73a3f24ccc44fad7dfa34b2a50e4e1478e8d26a9
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-17 18:06:42 -0400

    seed file updated with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Post.destroy_all
-Comment.destroy_all 
+Comment.destroy_all
+
+trip = Post.create(title: "Top 10 Places to Visit this Summer", author: "Alexa Klein", photo_url: "")
+fun = Post.create(title: "Top 5 Activities in D.C. this Spring", author: "El Presidente", photo_url: "")
+eat = Post.create(title: "Top DC Restaurants and Bars", author: "Hungry Sam", photo_url: "")
+history = Post.create(title: "7 Historical DC Facts you Don't Know!", author: "Uncle Sam", photo_url:"")
