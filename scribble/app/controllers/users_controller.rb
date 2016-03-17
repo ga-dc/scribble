6d3daac63ce585829f40fd5608e303d15cf41265
Patrick Moran <patrick_moran@mac.com>
2016-03-17 13:51:09 -0400

    added new user form

--- scribble/app/controllers/users_controller.rb
+++ scribble/app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create!(user_params)
+    redirect_to posts_url
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
