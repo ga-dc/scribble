25dedaca68ddd33b48abe4512d5bcd151de8244c
Ryan Krolick <rkrolick@gmail.com>
2016-03-17 23:14:56 -0400

    final functionality added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments dependent: :destroy
+  # dependent: :destroy tells ruby to destroy all of this post's comments if the
+  # post is removed.
 end
