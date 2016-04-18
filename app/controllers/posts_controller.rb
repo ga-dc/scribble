20cfeaeb66244c2f145325d707d14f9613a630a7
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-18 19:27:48 -0400

    adds create and delete features for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   def new
+    @post = Post.new()
   end
   def create
+    @post = Post.create!(post_params)
+  redirect_to @post
+  end
+  def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to posts_path
+    end
+  private
+  def post_params
+      params.require(:post).permit(:title, :text)
   end
 end
