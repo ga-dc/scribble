eaf96c7740457b671300dc466e0cd05f26b038b6
Spicy Legato <jpqreichard@gmail.com>
2016-04-19 13:57:11 -0400

    adds all RESTful routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,19 @@
 class CommentsController < ApplicatonController
 
-  def index
-    @comments = Comments.all
-  end
 
-  def show
-    @comments = Comments.find(params[:id])
-  end
+def new
+end
+
+def create
+end
+
+def update
+end
+
+def edit
+end
+
+def destroy
+end
 
 end
