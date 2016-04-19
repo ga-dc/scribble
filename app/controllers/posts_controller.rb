89d3129bdfe159d609260f11f9710640d086bff8
Leland Jordon <lelandjordon@mac.com>
2016-04-19 16:57:14 -0400

    Forgot what I changed nine hours ago.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,38 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
+  end
+
   title
   content
-  
+  author
 
+  private
+  def post_params
+    params.require(:post).permit(:body, :author)
+  end
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419035428) do
+ActiveRecord::Schema.define(version: 20160419142837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160419035428) do
     t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
