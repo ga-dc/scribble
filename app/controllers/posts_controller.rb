d6aaa03636b434ad2caba7b2e6740ae3b3c36b91
Mary Griffus <mary.e.shultz@gmail.com>
2016-04-19 18:50:26 -0400

    added validation for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create!(post_params)
-    redirect_to @post
+    @post = Post.new(post_params)
+    if @post.valid?
+      flash[:notice] = "Post added"
+      @post.save
+      redirect_to @post
+    else
+      flash[:alert] = "Post rejected"
+      render :new
+    end
   end
 
   def edit
@@ -18,8 +25,15 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to posts_path()
+    test_post = Post.new(post_params)
+    if test_post.valid?
+      flash[:notice] = "Edit complete"
+      @post.update(post_params)
+      redirect_to post_path()
+    else
+      flash[:alert] = "Edits rejected"
+      render :edit
+    end
   end
 
   def show
@@ -30,13 +44,16 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
+    @post.tags.each do |t|
+      t.destroy
+    end
     @post.destroy
     redirect_to posts_path()
   end
 
   private
   def post_params
-    params.require(:post).permit(:body, :title, :author)
+    params.require(:post).permit(:body, :title, :author, :all_tags)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :comments, only: [:new, :create, :edit, :update, :destroy]
+
+  resources :categories, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
