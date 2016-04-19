2c4d1615564fdd30e8a5edbe75a01bfe735ef73f
Wes Reed <wesley.a.reed@gmail.com>
2016-04-18 22:54:54 -0400

    Add update action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :text)
