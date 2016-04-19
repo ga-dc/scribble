8eae37aec3b62209b395573fb9c4368c11a54f02
Zaneh Williams <zanehmw@gmail.com>
2016-04-19 08:22:26 -0400

    information not rendering on show page, and weird things rendering on index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:name, :nationality, :photo_url)
+    params.require(:post).permit(:title, :body)
   end
 
 end
