0c5f4100ddab2304f6128e6c95666bfa91c7db05
Spicy Legato <jpqreichard@gmail.com>
2016-04-19 13:57:21 -0400

    adds all RESTful routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params[:post])
+    redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
 end
