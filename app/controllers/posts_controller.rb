af88bcf26c4641c5ebc7168a0107530f47038918
Wes Reed <wesley.a.reed@gmail.com>
2016-04-19 19:32:17 -0400

    Add flash message to destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.destroy
 
-    redirect_to posts_path
+    redirect_to posts_path, alert: "#{@post.title} has been destroyed."
   end
 
   private
