e8768a7c4507f7a86fd9c908d9a6c26e0a99ea82
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-20 07:59:00 -0400

    adds show page and edit form for comments; w/o update feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,19 @@
 class CommentsController < ApplicationController
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment= @post.comments.update(comment_params)
+    redirect_to post_comment_path(@comment)
+  end
 
   def create
     @post = Post.find(params[:post_id])
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   def new
     @post = Post.new()
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -16,6 +19,20 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
       @post = Post.find(params[:id])
       @post.destroy
@@ -25,4 +42,5 @@ class PostsController < ApplicationController
   def post_params
       params.require(:post).permit(:title, :text)
   end
+
 end
