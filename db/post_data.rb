a7077880ac4d9f667254cf4cced08f1b07a8dcda
Leland Jordon <lelandjordon@mac.com>
2016-04-20 00:12:11 -0400

    FIXED the database!  I think...

--- db/post_data.rb
+++ db/post_data.rb
@@ -4,17 +4,17 @@ def get_post_data
                 title: "Dug's Big Day!",
                 body:  "I went to the park and chased the bird with Alpha!",
                 author: "Dug",
-              },
+                },
     "foundabird" => {
                 title: "I found the bird!",
                 body:  "I can't believe it!  I found a bird!",
                 author: "Dug",
-              },
+                },
     "readabook" => {
                 title: "I read a book.",
                 body: "I sat down in my favorite spot to read Robert Frost.",
                 author: "Dug",
-              }
+                }
     }
   return post_data
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420011850) do
+ActiveRecord::Schema.define(version: 20160419142837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160420011850) do
   create_table "comments", force: :cascade do |t|
     t.string   "body"
     t.string   "author"
+    t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
