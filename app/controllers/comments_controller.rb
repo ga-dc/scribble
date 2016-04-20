92adb104faa4e2e77c7d91b027fc83fc617d2baa
Zaneh Williams <zanehmw@gmail.com>
2016-04-20 16:49:22 -0400

    finished edit and delete features for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,36 @@ class CommentsController < ApplicationController
     @comment = Comment.create(cleaned_params)
     # @post = Post.find(params[:comment][:post_id])
     redirect_to @comment.post
-
   end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    cleaned_params = params.require(:comment).permit(:comment, :post_id)
+    @comment = Comment.find(params[:id])
+    if @comment.update(cleaned_params)
+      redirect_to @comment.post
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    @comment = Comment.find(params[:post_id])
+    # @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to @comment.post
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:post_id, :comment)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'comments/create'
+
 
   get "welcome/index"
   root "welcome#index"
   resources :posts
-  resources :comments, only: [:create]
+  resources :comments
 end
