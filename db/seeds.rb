4fe27045396f7df28d3f7d216c70c20f082b156a
Matt <mvaden08@gmail.com>
2016-04-20 04:57:21 -0400

    Created models, added seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Models: `Post` and `Comment`
+
+require_relative './post_data.rb'
+require_relative './comment_data.rb'
+
+Post.destroy_all
+Comment.destroy_all
+
+post_data = get_post_data()
+comment_data = get_comment_data()
+
+post_data.each_pair do |post_name, posts|
+  info = post_data[post_name]
+  current_post = Post.create!({
+    username:     info[:username],
+  })
+
+  comments.each do |comment|
+    Comment.create!({
+      title:      comment[:title],
+      username:   info[:username]
+    })
+  end
+end
