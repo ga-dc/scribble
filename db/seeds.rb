a4fcec8a46a730eea4a33ca27e66a0617b603ddf
Matt <mvaden08@gmail.com>
2016-04-20 08:47:15 -0400

    Got help, used Rails migrate to fix a few things, modified schema, seeds and  models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Models: `Post` and `Comment`
-
-require_relative './post_data.rb'
-require_relative './comment_data.rb'
-
 Post.destroy_all
 Comment.destroy_all
 
-# post_data = get_post_data()
-# comment_data = get_comment_data()
+post1 = Post.create(title: "WDI Day One", body: "Everything is scary and amazing, I'm in for a long journey")
+post2 = Post.create(title: "First Homework Assignment", body: "My brain hurts, omg someone save me!")
+post3 = Post.create(title: "Project Number 1", body: "How did I do this? I'm surprised haha")
 
-# user.create ([
-#   {user:"player_one"},
-#   {user:"player_two"}
-#   ])
-
-post_data.each do |post_name, posts|
-  info = post_data[post_name]
-  current_post = Post.create!({
-    username:     info[:username],
-    text:         body[:text],
-    post_id:      integer[:post_id],
-    created_at:   create[:created_at],
-    updated_at:   update[:updated_at]
-  })
-
-  comments.each do |comment|
-    Comment.create!({
-    title:        comment[:title],
-    text:         body[:text],
-    user_id:      integer[:user_id],
-    created_at:   create[:created_at],
-    updated_at:   update[:updated_at]
-    })
-  end
-end
+Comment.create(body: "wow that's so cool", post: post1)
+Comment.create(body: "wow that's so pretty", post: post1)
+Comment.create(body: "hahaha", post: post2)
+Comment.create(body: "you are amazing that's why", post: post3)
