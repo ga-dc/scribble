2e6887ff7c7797cfdfd4a4c86ef1b4eaee26d6a1
Leland Jordon <lelandjordon@mac.com>
2016-04-19 21:01:56 -0400

    Slapped together comment and post data for seeding.

---
+++ db/migrate/post_data.rb
@@ -0,0 +1,20 @@
+def get_post_data
+  post_data = {
+    "dugsbigday" => {
+                title: "Dug's Big Day!",
+                body:  "I went to the park and chased the bird with Alpha!",
+                author: "Dug",
+              },
+    "foundabird" => {
+                title: "I found the bird!",
+                body:  "I can't believe it!  I found a bird!",
+                author: "Dug",
+              },
+    "readabook" => {
+                title: "I read a book.",
+                body: "I sat down in my favorite spot to read Robert Frost.",
+                author: "Dug",
+              }
+    }
+  return post_data
+end
