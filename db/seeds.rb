05be13cf278a5cac979acc62b8816ce96570fd43
Wes Reed <wesley.a.reed@gmail.com>
2016-04-20 22:51:34 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+
+post1 = Post.create(title: "Spring", text: "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.")
+post2 = Post.create(title: "MTV", text: "DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps.")
+post3 = Post.create(title: "Wiz", text: "A wizard’s job is to vex chumps quickly in fog. Watch Jeopardy!")
