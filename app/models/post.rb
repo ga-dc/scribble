55610e54a3448e878e9de88fae3670e936e62260
Wes Reed <wesley.a.reed@gmail.com>
2016-04-20 21:17:51 -0400

    Migrate category table and update post model with association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   has_many    :comments, dependent: :destroy
+  has_many    :categories
   validates   :title, presence: true,
                     length: {minimum: 5}
   validates   :text, presence: true,
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420000547) do
+ActiveRecord::Schema.define(version: 20160421011310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "categories", ["post_id"], name: "index_categories_on_post_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -33,5 +42,6 @@ ActiveRecord::Schema.define(version: 20160420000547) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "categories", "posts"
   add_foreign_key "comments", "posts"
 end
