f89c9dc00f69f272f8fabcb7582823e57a99a886
Wes Reed <wesley.a.reed@gmail.com>
2016-04-20 21:14:17 -0400

    Fix category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-  has_many :post
+  belongs_to :post
 end
--- db/migrate/20160421011310_create_categories.rb
+++ db/migrate/20160421011310_create_categories.rb
@@ -1,6 +1,8 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
+      t.string :name
+      t.references :post, index: true, foreign_key: true
 
       t.timestamps null: false
     end
