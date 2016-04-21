06905014f0ba879e9efd2d4db7de4d5d7bb30feb
Wes Reed <wesley.a.reed@gmail.com>
2016-04-20 23:16:49 -0400

    Update post title validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many    :tags, dependent: :destroy
   has_many    :categories, through: :tags
   validates   :title, presence: true,
-                    length: {minimum: 5}
+                    length: {minimum: 2}
   validates   :text, presence: true,
                     length: {minimum: 5, maximum: 140}
 end
