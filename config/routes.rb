f63dcec16a8c57058ae8a0eafa36ee2f8944b225
Mara Sanders <sandersmara@gmail.com>
2016-06-29 19:58:07 -0400

    added some user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  resources :users
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -56,4 +59,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
