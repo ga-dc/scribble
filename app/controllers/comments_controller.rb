dc9af0ca824c24e24e57235fabbcf72e329796f5
Matthew Scott <matthewruelscott@gmail.com>
2016-06-28 21:04:36 -0400

    Fixed comment editing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,17 @@
 class CommentsController < ApplicationController
   def new
+    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
   def create
-    @comment = Comment.create!(comment_params)
-    redirect_to @comment
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create!(comment_params.merge(post: @post))
+    redirect_to post_path(@post)
   end
 
   def destroy
@@ -15,17 +21,18 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
   end
 
   def update
     @comment = Comment.find(params[:id])
     @comment.update(comment_params)
-    redirect_to @comment
+    redirect_to @comment.post
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :author)
   end
 end
