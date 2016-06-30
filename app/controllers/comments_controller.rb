e35385985544ae34b588b86b626fc182e0d7b84d
Kristin Pasive <kristin.pasive@gmail.com>
2016-06-29 20:10:32 -0400

    integrated post and comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,34 @@
+class CommentsController < ApplicationController
+  def index
+    @comment = Comment.all
+  end
+  def new
+    @comment = Comment.new
+  end
+  def create
+    @comment = Comment.create!(comment_params)
+    redirect_to comments_path
+  end
+  def show
+    @comment = Comment.find(params[:id])
+  end
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to comments_path
+  end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to comments_path
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:title, :body)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628180420) do
+ActiveRecord::Schema.define(version: 20160629231906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
+    t.string  "title"
+    t.string  "body"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
