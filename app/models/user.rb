8318a5266ed1147b12368e9f3304bba74332fd8d
Andrew Young <prince.of.utah@gmail.com>
2016-06-29 22:30:45 -0400

    tried to add the templates for the comments and posts of the view folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
+  has_many :comments
 end
