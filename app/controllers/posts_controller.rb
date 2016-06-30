8f55a216354274b130249e2d7e36f6447f113b6c
Andrew Young <prince.of.utah@gmail.com>
2016-06-29 21:44:50 -0400

    added the def templates for the controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,23 @@
 class PostsController < ApplicationController
+  def index
 
+   end
+   def show
+
+   end
+   def edit
+
+   end
+   def new
+
+   end
+   def create
+
+   end
+   def update
+
+   end
+   def destroy
+
+   end
 end
