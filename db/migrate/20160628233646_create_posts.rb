c74fd8c7d806ff9e53a90b3bbf8c6ea530abfeb4
Denzel Christopher Adams <Blvckyuppie@gmail.com>
2016-06-30 08:58:41 -0400

    thursday

--- db/migrate/20160628233646_create_posts.rb
+++ db/migrate/20160628233646_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.text   :body
+      t.integer :post_id 
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,15 @@ ActiveRecord::Schema.define(version: 20160628233655) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
