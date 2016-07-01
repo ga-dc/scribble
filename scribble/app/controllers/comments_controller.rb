1955ff65e106cb33eeee9eed1f6304ddec9d529c
Chris Gardner <ctgardner89@gmail.com>
2016-07-01 08:15:42 -0400

    stopping point

--- scribble/app/controllers/comments_controller.rb
+++ scribble/app/controllers/comments_controller.rb
@@ -1,3 +1,11 @@
 class CommentsController < ApplicationController
-  
+  def new
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+  def create
+    @post = Post.find(params[:id])
+    @comment = @post.comments.create(params[:id])
+    redirect_to @post
+  end
 end
