96f204b75fe87afab38c418e45b4c4e05d2c35f6
Domonique Griffin <griffind2009@gmail.com>
2016-10-10 14:24:20 -0400

    new created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+def show
+  @post = Post.find(params[:id])
+end
+
+def create
+end
+
+def destroy
+end
+
+private
+def post_params
+  
+end
+
 end
