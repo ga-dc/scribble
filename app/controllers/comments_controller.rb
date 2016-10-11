3c84544e5e69c4e78169f8a5982b74339016e18f
Domonique Griffin <griffind2009@gmail.com>
2016-10-11 19:30:18 -0400

    can create a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,16 @@
-class Comments < ApplicationController
-  def index
+class CommentsController < ApplicationController
+
+  def create
+     @post = Post.find(params[:post_id])
+     @comment = @post.comments.create(comment_params)
+     redirect_to post_path(@post)
+   end
+
+
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
   end
 end
