8c555fdaced3e47328ed15c78b18059ee155d612
Latoya Watson <latoyamwatson@gmail.com>
2016-10-12 19:33:30 -0400

    almost done with posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,46 @@
 class PostsController < ApplicationController
 
 def index
-@posts = Post.all
+  @posts = Post.all
 end
 
+def show
+  @post = Post.find(params[:id])
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.new(post_params)
+
+  if @post.save
+    redirect_to @post
+  else
+    render 'new'
+  end
+end
+
+def update
+  @post = Post.update(params[:id])
+
+  if @post.update(post_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+end
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
 
 end
