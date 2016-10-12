e5213f1c20898ae8bb24e6a32c50e080462f4f9c
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-10-12 14:02:04 -0400

    updates

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,25 @@
 class CommentsController < ApplicationController
 
-  
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+  def edit
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,5 +16,24 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-  
+
+  def create
+    @post = Post.create!(post_params.merge(user: current_user))
+    redirect_to post_path(@post)
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
