e5262b3a72abde5140644d8c60a59cce97e1dbf5
Nick Oki <nick.oki@live.com>
2016-10-12 19:10:28 -0400

    Comment New and Create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,25 @@ class CommentsController < ApplicationController
   #index
   #show
   #new
+  def new
+    @post = Post.find params[:post_id]
+    @comment = Comment.new
+  end
+
   #create
+  def create
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.create!(comment_params)
+
+    redirect_to post_path(@post)
+  end
   #edit
   #update
   #destroy
 
   #private
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
