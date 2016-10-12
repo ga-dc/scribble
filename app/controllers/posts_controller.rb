70bcccdef9bd515857fe8efe413cd7604c640940
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-10-12 18:40:30 -0400

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,18 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_user!
-
   def index
     @posts = Post.all
   end
-
   def show
     @post = Post.find(params[:id])
   end
-
   def edit
     @post = Post.find(params[:id])
   end
-
   def new
     @post = Post.new
   end
-
   def create
-    @post = Post.create(post_params)
+    @post = Post.create!(post_params)
     redirect_to post_path(@post)
   end
   def update
@@ -26,7 +20,6 @@ class PostsController < ApplicationController
     @post.update(post_params)
     redirect_to post_path(@post)
   end
-
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
