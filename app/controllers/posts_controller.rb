af920a3bb1afdae98447a995221933f76377ea66
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-10-12 13:50:09 -0400

    updates

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  # before_action :authenticate_user!
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+end
