1b195af2ad7d9079380425a27586a70b26c77201
Andrew Whitley <awhitley1233@gmail.com>
2016-10-12 18:12:32 -0400

    reforked from wdi

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def new
     @post = Post.find(params[:post_id])
     @comment = Comment.new
@@ -6,7 +8,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(strong_params)
+    @comment = @post.comments.create!(strong_params.merge(user: current_user))
     redirect_to post_path(@post)
   end
 
@@ -18,14 +20,22 @@ class CommentsController < ApplicationController
   def update
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
-    @comment.update(strong_params)
+    if @comment.user == current_user
+      @comment.update(strong_params)
+    else
+      flash[:alert] = "You are not authorized to edit this comment"
+    end
     redirect_to post_path(@post)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
-    @comment.destroy
+    if @comment.user == current_user
+      @comment.destroy
+    else
+      flash[:alert] = "You are not authorized to delete this comment"
+    end
     redirect_to post_path(@post)
   end
 
