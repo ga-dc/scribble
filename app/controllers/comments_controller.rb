49abaae4c1c1f1a2ac8ec2a2a751a1981f0a17fd
manish pradhan <pradhanmanish33@gmail.com>
2016-10-13 01:10:08 -0400

    commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def updated
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to @comment.post
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:blog, :name)
+  end
+end
