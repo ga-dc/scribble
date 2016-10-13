4afb4e897bc4986a0821d7b72b1a8d6c138c4495
Michael Rubin <michael@rubinsworld.com>
2016-10-12 20:52:00 -0400

    MVP done No Bonus or CSS yet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,15 @@ def show
   @post = Post.find(params[:id])
 end  #show
 
+def edit
+    @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to @post
+end
 
 def new
   @post = Post.new
@@ -19,6 +28,20 @@ def create
   @post.save
   redirect_to @post
 end
+
+def destroy
+ @post = Post.find(params[:id])
+
+
+ @post.comments.each do |comment|
+   comment.destroy
+ end
+
+ @post.destroy
+ redirect_to posts_path
+end
+
+
 
 private
   def post_params
