8c292cb27756c4b98b6a05234adfbbbe3ca04c8d
Khoi Le <khoileee89@gmail.com>
2016-10-13 08:29:10 -0400

    started integrating more models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  has_many :categorys
+  has_many :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012025452) do
+ActiveRecord::Schema.define(version: 20161012234310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_categories_on_post_id", using: :btree
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string  "body"
@@ -29,4 +36,16 @@ ActiveRecord::Schema.define(version: 20161012025452) do
     t.string  "img_url"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_tags_on_category_id", using: :btree
+    t.index ["post_id"], name: "index_tags_on_post_id", using: :btree
+  end
+
+  add_foreign_key "categories", "posts"
+  add_foreign_key "tags", "categories"
+  add_foreign_key "tags", "posts"
 end
