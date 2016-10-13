13ca526d4749dd945af6e0545ef548020d0f8e66
Latoya Watson <latoyamwatson@gmail.com>
2016-10-12 23:12:49 -0400

    finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,12 +13,19 @@ end
 
 def edit
   @post = Post.find(params[:post_id])
-  @comment = Comment.find(params[:id])
+  @comment = @post.comments.find(params[:id])
 end
 
 def update
   @post = Post.find(params[:post_id])
   @comment = @post.comments.update(comment_params)
+  redirect_to post_path(@post)
+end
+
+def destroy
+  @post = Post.find(params[:post_id])
+  @comment = Comment.find(params[:id])
+  @comment.destroy
   redirect_to post_path(@post)
 end
 
