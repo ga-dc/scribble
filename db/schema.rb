a058171c69eb98e55d424a1a4a96f982ca3ec8e9
Latoya Watson <latoyamwatson@gmail.com>
2016-10-12 20:38:43 -0400

    updated schema via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012182442) do
+ActiveRecord::Schema.define(version: 20161013002857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161012182442) do
     t.string   "email"
     t.datetime "timestamp"
     t.integer  "post_id"
+    t.string   "body"
     t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
   end
 
