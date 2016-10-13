3df34baa290bee6509e3054ae1dc7923d790256a
Latoya Watson <latoyamwatson@gmail.com>
2016-10-12 22:44:06 -0400

    still working on comments on individual posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,26 @@ def new
 end
 
 def create
-  @post = Post.find
+  @post = Post.find(params[:post_id])
+  @comment = @post.comments.create(comment_params)
+  redirect_to post_path(@post)
 end
 
 def edit
+  @post = Post.find(params[:post_id])
   @comment = Comment.find(params[:id])
 end
 
+def update
+  @post = Post.find(params[:post_id])
+  @comment = @post.comments.update(comment_params)
+  redirect_to post_path(@post)
+end
+
+
+private
+def comment_params
+  params.require(:comment).permit(:body)
+end
 
 end
