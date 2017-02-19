4a5313f6b45919bfb9c2ad38b2feac2687b6eab8
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-19 09:42:34 -0500

    Revert "added index route"
    This reverts commit b38867499e7c141f51602149f14d363bbd8e85ad.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
-
-  resources :posts do
-  resources :comments
-
-end
-
-#posts routes (for my reference)
-
-get       '/posts',       to: 'posts#index'
-
-#comments routes (for my reference)
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
