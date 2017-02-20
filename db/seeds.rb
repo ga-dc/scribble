f65b2f339edb1b6ee5c71b75197254cdb38c21ff
Will Hawkins <willhawkins123@yahoo.com>
2017-02-19 21:47:08 -0500

    Rad CSS and sillier seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,16 @@
 Post.destroy_all
 Comment.destroy_all
 
-first = Post.create(title: 'Test Post 1', author: 'Will Hawkins', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', likes: 1)
-second = Post.create(title: 'Test Post 2', author: 'William Hawkins', content: 'Other stuff.', likes: 0)
-third = Post.create(title: 'Test Post 3', author: 'Bill Hawkins', content: 'Even more other stuff.', likes: 0)
+first = Post.create(title: 'Llama Sounds', author: 'Liam Hawkins', content: 'No one knows what sound llamas make', likes: 2)
+second = Post.create(title: 'Sheep Sounds', author: 'Bill Hawkins', content: 'Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep Sheep', likes: 67)
+third = Post.create(title: 'Cow Sounds', author: 'William Hawkins', content: 'MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO', likes: 0)
+fourth = Post.create(title: 'Chicken Sounds', author: 'Will Hawkins', content: 'Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk Bawk', likes: 10)
 
-first.comments.create(content: 'I think this post is great.', author: 'Will Yum', likes: 0)
-first.comments.create(content: 'I think this post is not great.', author: 'Will Bum', likes: 3)
-first.comments.create(content: 'I am a cat.', author: 'William', likes: 0)
-second.comments.create(content: "I'm the only comment on this post.", author: 'Will Gum', likes: 4)
-third.comments.create(content: 'I think this post is boring.', author: 'Will Hawkins', likes: 1)
-third.comments.create(content: 'I am also a comment.', author: 'Will Hawkins', likes: 0)
+first.comments.create(content: "So profound!", author: "Will Dawkins", likes: 1)
+first.comments.create(content: "#this", author: "Phil Hawkins", likes: 1)
+second.comments.create(content: "I don't think sheep talk like pokemon...", author: 'Will Dawkins', likes: 1)
+second.comments.create(content: "I'm so sheepy - I mean SLEEPY - now.", author: 'Gill Hawkins', likes: 0)
+third.comments.create(content: "Wow! That is a lot of O's!", author: 'Willie Hawkins', likes: 4)
+fourth.comments.create(content: 'I think this post is not great.', author: 'Will Yum', likes: 3)
+fourth.comments.create(content: 'I am a cat.', author: 'William', likes: 0)
+fourth.comments.create(content: 'Why are all of these posts just animal sounds?', author: 'Bill Hawkins', likes: -2)
