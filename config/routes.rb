468bceba430a49ca82ffff85c281de8a468f8ef8
Eva Lai <evalai.dc@gmail.com>
2017-02-21 14:27:56 -0500

    working page for the posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'posts#index'
+
+resources :posts
+  resources :comments
+
+
+
+  # get '/posts'        => 'posts#index'
+  # get '/posts/new'    => 'posts#new'
+  # get '/posts/:id'    => 'posts#show'
+  # post '/posts'       => 'posts#create'
+  # get '/posts/:id/edit' => 'posts#edit'
+  # patch '/posts/:id'   => 'posts#update'
+  # delete '/posts/:id'  => 'posts#destroy'
 end
