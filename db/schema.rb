e410812996c57214c5e5404f4f49711641b46546
Wes <wes.coffay@gmail.com>
2017-02-21 18:45:54 -0500

    debugged semantics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217213043) do
+ActiveRecord::Schema.define(version: 20170221232232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170217213043) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "content"
+    t.integer  "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 20170217213043) do
     t.string   "content"
   end
 
+  add_foreign_key "comments", "posts"
 end
