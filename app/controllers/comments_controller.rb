6df23173af839470b401bbe917d480fcfd7d8aba
Chris Wightman <wightman_christopher@bah.com>
2017-02-21 16:45:58 -0500

    Commit for safety. Still not working.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+
+  # new
+  def new
+    @comment = Comment.new
+
+    redirect_to post_path(@post)
+  end
+
+  # create
+  def create
+    @comment = Comment.create!(post_params)
+
+    redirect_to post_path(@post)
+  end
+
+  # destroy
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
+  private
+  def artist_params
+    params.require(:post).permit(:author, :post_title, :post_text, :created_on, :updated_on)
+  end
+end
 end
