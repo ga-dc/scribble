053530b6527af7662be49a27d49050f33d6d5e6e
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-22 14:17:03 -0500

    delete works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
     @post = Post.create!(post_params)
     redirect_to post_path(@post)
@@ -20,6 +24,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
 end
