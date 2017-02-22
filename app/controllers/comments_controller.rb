176215b0e2547abcd462b6b6c49e1e17cf5d31fb
Nat Carter <n.goldman.carter@gmail.com>
2017-02-22 17:05:57 -0500

    finished comments CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,13 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text)
