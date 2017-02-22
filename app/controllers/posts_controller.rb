ebfaee34166d2a8d44d3e5c239adcf396491f291
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-02-22 14:03:15 -0500

    index show edit and new for post done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,23 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create!(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
 end
