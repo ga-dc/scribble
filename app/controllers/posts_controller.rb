6d79c8ece14ab0a4cf26f20877209586caf950d0
Bedig Galladian <bgalladian@gmail.com>
2017-02-22 19:48:17 -0500

    got CRUD functionality for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,21 @@
 class PostsController < ApplicationController
 
+
   def index
     @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.create!(post_params)
-    redirect_to "/posts"
+    redirect_to '/posts'
   end
 
   def edit
@@ -29,8 +30,17 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
-    @post.destroy_all
-    redirect_to "/posts"
+    @post.destroy
+    redirect_to '/posts'
   end
+
+
+
+private
+def post_params
+  params.require(:post).permit(:title, :author, :content)
+
+end
+
 
 end
