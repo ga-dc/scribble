99c77cb2d1ea7c6378bf2c1c19181ee5accc6785
xgironx <xgironx@gmail.com>
2017-02-23 23:32:51 -0500

    editing and creating posts/comments with nested resources i found really tough.  i took some screenshots to discuss problems with that and with problems i intro'd with incomplete User / Devise implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,6 @@
 Rails.application.routes.draw do
-  get "posts" => "posts#index"
-  get "posts/new" => "posts#new"
-  # post "posts" => "posts#create"
-  get "posts/:id" => "posts#show"
-  # get "posts/:id/edit" => "posts#edit"
-  # put "posts/:id" => "posts#update"
-  # delete "posts/:id" => "posts#destroy"
-  #
-  # get "comments" => "comments#index"
-  # get "comments/new" => "comments#new"
-  # post "comments" => "comments#create"
-  get "comments/:id" => "comments#show"
-  # get "comments/:id/edit" => "comments#edit"
-  # put "comments/:id" => "comments#update"
-  # delete "comments/:id" => "comments#destroy"
-
-
-########################################
-# 201702161222L EL JUEVES  GIRO
-  # get "hello/:name" => "hello#{show}"
+  root to: "posts#index"
+  resources :posts do
+    resources :comments
+  end
 end
