5398b12103baf122d71a411f63fe0765185d3fa4
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-24 08:26:50 -0500

    updated posts and comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,33 @@
 class CommentsController < ApplicationController
 
+def show
+  @comment = Comment.find(params[:id])
+end
 
+def new
+  @comment = Comment.new
+end
 
+def edit
+  @post = Post.find(params[:artist_id])
+  @comment = Comment.find(params[:id])
+end
 
+def create
+  @comment = Comment.create!(comment_params)
+  redirect_to post_comment_path(@comment.post)
+end
 
-  
+def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@comment.post)
+end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author, :content, :post_id)
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,33 @@ end
 
 def show
   @post = Post.find(params[:id])
-  @comment = Comment
+  @comment = Comment.new
+
 end
 
+def new
+  @post = Post.new
+end
 
+def edit
+  @post = Post.find(params[:id])
+end
 
+def create
+  @post = Post.create!(post_params)
+  redirect_to post_path(@post)
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to posts_path
+end
+
+private
+def post_params
+  params.require(:post).permit(:title, :text, :img_url)
+end
 
 end
