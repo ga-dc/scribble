91eae635eb75cd92db2f57a2721a275d22e6182a
Andy Flickinger <atflick@gmail.com>
2017-02-23 20:47:36 -0500

    turning in hw

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,12 +2,10 @@
 $(document).scroll(function(){
   if($(this).scrollTop() > 10) {
     $("header").css({"height" : "0px"});
-    $("body").css({"padding-top" : "110px"});
     $("nav").css({"top" : "0px"})
     $(".hidden-logo").css({"opacity" : 1})
   } else {
     $("header").css({"height" : "200px"});
-    $("body").css({"padding-top" : "250px"});
     $("nav").css({"top" : "200px"})
     $(".hidden-logo").css({"opacity" : 0})
   }
