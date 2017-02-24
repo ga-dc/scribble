29b5a1ed374e904a8984e410234c762d8a355ced
Erica Norby <ericanorby@gmail.com>
2017-02-23 19:46:52 -0500

    finished basic requirements

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,9 +11,21 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def destroy
+  def edit
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
   end
