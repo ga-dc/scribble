05afbe048dbc0aac1e7aad1fbec996b334c0163f
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-20 18:30:59 -0400

    nest comment resource post resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  root 'posts#index'
-  get 'comment/index'
+  root to: 'posts#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
 end
