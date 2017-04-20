9ab6b7c047648757942bec07bdcfd02ef9e5f5d7
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-20 18:31:35 -0400

    update comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.all
+  end
+
+  def show
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
   end
 end
