90e63e36412eb754356476216ed763cdfd6cc092
Erin <erinedawson@gmail.com>
2017-04-20 21:54:18 -0400

    add 'comments' to 'show' view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,19 @@ class PostsController < ApplicationController
   def show
     @posts = Post.find(params[:id])
   end
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @posts = Post.create!(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :author, :subject)
+  end
 end
