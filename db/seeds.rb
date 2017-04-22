6dc369f278635cfe98a2b15d40f0544d3d4d7bc3
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-04-22 16:26:30 -0400

    index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+Comment.destroy_all
+
+ick = Post.create(title: "Welcome Back!", author: "Ick", content: "Sexy Back!!!")
+kwon = Post.create(title: "I'm Back!", author: "Kwon", content: "Why??")
+sung = Post.create(title: "I'm noob", author: "Sung", content: "What?")
