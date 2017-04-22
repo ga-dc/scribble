f9f7b60635d46b7e37dcdb39107f8b3446a4bfb6
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-04-22 17:13:12 -0400

    Delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,14 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post)
     end
 
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
+    end
+
+
     private
     def comment_params
         params.require(:comment).permit(:author, :content)
