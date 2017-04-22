f6d2acb6c5ce1cf55dfda52c86d3f08d368c98e7
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-04-22 17:00:39 -0400

    Create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,15 @@ class CommentsController < ApplicationController
         @post = Post.find(params[:post_id])
         @comment = @post.comments.new
     end
+
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        redirect_to post_path(@post)
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:author, :content)
+    end
 end
