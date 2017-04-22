c689badf337ffbc9e839cb24171d792397c6031a
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-04-22 17:09:01 -0400

    Update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,18 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post)
     end
 
+    def edit
+        @post = Post.find(params[:post_id])
+        @comment = Comment.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.update(comment_params)
+        redirect_to post_path(@post)
+    end
+
     private
     def comment_params
         params.require(:comment).permit(:author, :content)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,16 @@ class PostsController < ApplicationController
         redirect_to post_path(@post)
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.create!(post_params)
+        @post.update(post_params)
+        redirect_to post_path(@post)
+    end
+
     private
     def post_params
         params.require(:post).permit(:title, :content)
