e705d6372fa8eaae423124a3c32b5eec9e7ce25e
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-04-23 17:22:42 -0400

    Comments does not render

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,40 @@
 class PostsController < ApplicationController
-  # actions will go here
-  def index
-    @posts = Post.all
-  end
 
-#   def new
-#     @post = Post.new
-#   end
-#
-#   def create
-#    @post = Post.create(post_params)
-#
-#    redirect_to new_post_path(@post)
-# end
-#
-# def show
-#     @post = Post.find(params[:id])
-#   end
+	def index
+		@posts = Post.all
+	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
 
+	def new
+		@post = Post.new
+	end
 
-  # 
-  #  private
-  # def post_params
-  #   params.require(:post).permit(:title, :discription)
-  # end
+	def create
+		@post = Post.create!(post_params)
+		redirect_to "/posts/#{@post.id}"
+	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
 
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:title, :category, :photo_url, :text)
+	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  validates :title, :description, {presence: true}
+  # validates :title, :description, {presence: true}
 end
