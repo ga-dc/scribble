cb90603a80095c5febfe904c1973ace12b23bb5c
Whitney <whitney.purdum@gmail.com>
2017-04-23 14:41:35 -0400

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
   end
 
+  def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.create!(post_params)
+  flash[:notice] = " Success! New artist was added."
+  redirect_to @post
+end
+
+private
+def post_params
+  params.require(:post).permit(:author, :title, :text, :category)
+end
+
 end
