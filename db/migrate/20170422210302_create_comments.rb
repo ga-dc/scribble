79590b43a9fed4e4d19b93efa0553e7a1f39b45b
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-23 20:03:03 -0400

    added comments

--- db/migrate/20170422210302_create_comments.rb
+++ db/migrate/20170422210302_create_comments.rb
@@ -1,9 +1,11 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.string :content
-      t.string :author_name
-      t.belongs_to :post_id
+      t.string :commenter
+      t.text :body
+      t.references :article, foreign_key: true
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422185702) do
+ActiveRecord::Schema.define(version: 20170422210302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,16 +23,13 @@ ActiveRecord::Schema.define(version: 20170422185702) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string  "content"
-    t.string  "author_name"
-    t.integer "post_id_id"
-    t.index ["post_id_id"], name: "index_comments_on_post_id_id", using: :btree
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "author_name"
-    t.string "content"
-  end
-
+  add_foreign_key "comments", "articles"
 end
