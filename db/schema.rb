d81756812ac136718ef25864438f59c514129b79
Erin <erinedawson@gmail.com>
2017-04-24 13:44:01 -0400

    drop posts table to fix 'table already exists' migration error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422214001) do
+ActiveRecord::Schema.define(version: 20170424173911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,15 +21,6 @@ ActiveRecord::Schema.define(version: 20170422214001) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "author"
-    t.string   "subject"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "body"
   end
 
 end
