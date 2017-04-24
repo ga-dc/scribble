22833b80b88bdc5ca093face412bdb121f02fd9d
Adam Hoff <swimmerdude79@comcast.net>
2017-04-24 18:53:06 -0400

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
