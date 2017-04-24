17b79b68d3d179157d4207064b1b057955317ac5
Andres Arango <andresarangov17@gmail.com>
2017-04-24 17:57:39 -0400

    create show. User sees posts and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+Comment.destroy_all
+
 posts = Post.create([
   {name: 'Adam Smith', title: 'Lorem Ipsum', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
   {name: 'Nikola Tesla', title: 'Lorem Ipsum', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+  ])
+
+comments = Comment.create([
+  {name: 'Thomas Edison', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', post_id: 1},
+  {name: 'Isaac Newton', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', post_id: 2}
   ])
