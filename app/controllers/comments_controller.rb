f8692c08608eba56a9bce1e289571247f1367b40
Andres Arango <andresarangov17@gmail.com>
2017-04-24 19:37:16 -0400

    user can create new posts and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
-  def index
-    
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :content)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,13 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(name: params[:post][:name], title: params[:post][:title], content: params[:post][:content])
+    redirect_to "/posts/"
+  end
 end
