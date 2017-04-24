31d518a4e7724dba0712905bb8b1f6a2b77b8a3f
Whitney <whitney.purdum@gmail.com>
2017-04-24 14:14:41 -0400

    update migrations

--- db/migrate/20170421123251_create_comments.rb
+++ db/migrate/20170421123251_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
     create_table :comments do |t|
       t.string :user
       t.string :text
-      t.integer :post_id
+      
     end
   end
 end
--- db/migrate/20170421124300_change_reference_in_posts.rb
+++
@@ -1,4 +0,0 @@
-class ChangeReferenceInPosts < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
