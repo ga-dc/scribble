02f80c8918abb895f783e754e52f97f5e3c2638f
Erin <erinedawson@gmail.com>
2017-04-24 18:33:07 -0400

    add new and edit views for comments.  nest comments inside paths

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,10 @@
-class commentsController < ApplicationController
+class CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+    @comments = Comment.find(params[:id])
+  end
 end
