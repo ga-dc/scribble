fb2d1008143e8fd22c69cffcda25b9485b787d6f
Jeong Hoon Koo <junghoone17@gmail.com>
2017-04-23 23:56:33 -0400

    completed Scribble w/o bonus

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,37 @@
 class CommentsController < ApplicationController
+
+
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(comment_params)
+    redirect_to @post, notice: "New comment added!"
+  end
+
+
+  def edit
+    @post = Post.find(params[:id])
+    @comment = Comment.find(params[:post_id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to post_path(@post), notice: "Comment is updated."
+  end
+
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
 end
