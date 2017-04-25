589f09344277f6ef961efdb8fe44ff2ab0f48a2a
Jonathan Wright <jonwright.va@gmail.com>
2017-04-25 07:42:19 -0400

    comment edit view now working, update still throwing error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,4 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  # GET /comments
-  # GET /comments.json
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  # GET /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
   def new
     @comment = Comment.new
   end
@@ -26,13 +12,13 @@ class CommentsController < ApplicationController
   # POST /comments
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.create!(comment_params)
     redirect_to post_path(@post)
   end
 
   # PATCH/PUT /comments/1
   def update
-   @post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
     @comment.update(comment_params)
     redirect_to post_path(@post)
@@ -47,13 +33,7 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.fetch(:comment, {})
+      params.require(:post).permit(:content)
     end
 end
