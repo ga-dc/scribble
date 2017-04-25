d69b4ae5132bc78f4fe234e0328e8a5bfc9f2283
Luisa Gonzalez <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-04-25 08:02:17 -0400

    scribble changes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create!(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+      @post.update(post_params)
+      redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+end
