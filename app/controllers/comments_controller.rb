fdd041a93b76a0ae0c140da8643225d60ad27615
Whitney <whitney.purdum@gmail.com>
2017-04-24 21:39:25 -0400

    update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
     @comment.destroy
+    flash[:notice] = "You've sucessfully deleted a comment."
     redirect_to post_path(@post)
   end
 
@@ -16,8 +17,24 @@ class CommentsController < ApplicationController
   def create
   @post = Post.find(params[:post_id])
   @comment = @post.comments.create(comment_params)
+  flash[:notice] = "Your comment has been added."
   redirect_to post_path(@post)
   end
+
+
+  def edit
+      @post = Post.find(params[:post_id])
+      @comment = Comment.find(params[:id])
+  end
+
+  def update
+      @post = Post.find(params[:post_id])
+      @comment = Comment.find(params[:id])
+      @comment.update(comment_params)
+      flash[:notice] = "You've sucessfully updated the comment."
+      redirect_to post_path(@post)
+  end
+
 
   private
   def comment_params
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,32 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = " Success! The post was deleted."
+    redirect_to root_path
+  end
+
   def show
       @post = Post.find(params[:id])
       @comment = @post.comments
   end
+
+
+  def edit
+      @post = Post.find(params[:id])
+    end
+
+
+    # update
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    flash[:notice] = " Success! The post was updated."
+    redirect_to post_path(@post)
+    end
+
 
   def new
   @post = Post.new
@@ -15,9 +37,11 @@ class PostsController < ApplicationController
 
 def create
   @post = Post.create!(post_params)
-  flash[:notice] = " Success! New artist was added."
+  flash[:notice] = " Success! Your post was added."
   redirect_to @post
   end
+
+
 
 private
 def post_params
