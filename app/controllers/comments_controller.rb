119c4ded4bb53acbc8bfe0bb6f0d84bd55977d18
amlak befekadu <amlakgirma@yahoo.com>
2017-04-25 07:00:11 -0400

    Scrible homework

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,23 @@ class CommentsController <ApplicationController
   def index
     @comments = Comment.all
   end
-   
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+  def create
+    puts params
+    @post = Post.find(params[:post_id])
+    @post.comments.create!(comment_params)
+    redirect_to post_path(@post)
+  end
+  def destroy
+   @comment = Comment[params[:id]]
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :text)
+  end
 end
