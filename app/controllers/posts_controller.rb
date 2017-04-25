39ffadf951762a6a685b9c36922af36edabeec95
Jonathan Wright <jonwright.va@gmail.com>
2017-04-24 22:42:59 -0400

    added comment views, controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,14 +49,14 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-#   private
-#     # Use callbacks to share common setup or constraints between actions.
-#     def set_post
-#       @post = Post.find(params[:id])
-#     end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
 
-#     # Never trust parameters from the scary internet, only allow the white list through.
-#     def post_params
-#       params.fetch(:post, {})
-#     end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.fetch(:post, {})
+    end
 end
