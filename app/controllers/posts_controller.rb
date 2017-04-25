f7178d4026cb52abde6f3a2b6688f88cb74fca80
Jonathan Wright <jonwright.va@gmail.com>
2017-04-24 22:31:30 -0400

    have index and show done for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
   end
 
   # GET /posts/new
@@ -19,6 +20,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
+    @post = Post.find(params[:id])
   end
 
   # POST /posts
@@ -32,16 +34,19 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    @post = Post.find(params[:id])
     @post.update(post_params)
-    respond_to do |format|
 
+    redirect_to post_path(@post)
+# @post.update(post_params)
+    # respond_to do |format|
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-
+    redirect_to post_path(@post)
   end
 
 #   private
@@ -54,4 +59,4 @@ class PostsController < ApplicationController
 #     def post_params
 #       params.fetch(:post, {})
 #     end
-# end
+end
