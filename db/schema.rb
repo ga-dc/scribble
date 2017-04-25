90d6f8989acf11bf1cace544c3e057da886195cf
Whitney <whitney.purdum@gmail.com>
2017-04-25 08:46:30 -0400

    begin bonus

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421123251) do
+ActiveRecord::Schema.define(version: 20170425124000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string  "user"
     t.string  "text"
     t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
   end
 
   create_table "posts", force: :cascade do |t|
@@ -28,4 +35,14 @@ ActiveRecord::Schema.define(version: 20170421123251) do
     t.string "category"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.integer "post_id",     null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_tags_on_category_id", using: :btree
+    t.index ["post_id"], name: "index_tags_on_post_id", using: :btree
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "tags", "categories"
+  add_foreign_key "tags", "posts"
 end
