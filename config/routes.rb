24541035bb5111751b4c4df7c26d6ed6d50275ee
Scott Pierce <scottpiercew@gmail.com>
2017-06-06 13:50:36 -0400

    Index done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root to: 'post#index'
+
+  get 'post/show'
+  get 'post/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
