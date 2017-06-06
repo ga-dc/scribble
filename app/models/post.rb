c1012a4a56cbefe354957353d416926bce86eca4
Tim Chase <tchase4544@gmail.com>
2017-06-05 21:38:44 -0400

    good to go no bonus

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
+	has_many :subjects, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,54 @@
 
 Post.destroy_all
 Comment.destroy_all
+Subject.destroy_all
+Tag.destroy_all
 
-post1 = Post.create(username:"Stanislaus",subject: "Hacks", content: "Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.")
-post2 = Post.create(username:"Gav",       subject: "Food", content: "Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.")
-post3 = Post.create(username:"Obadias",   subject: "Digital Photography", content: "Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus.")
-post4 = Post.create(username:"Dulci",     subject: "Food", content: "Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum.")
-post5 = Post.create(username:"Nanice",    subject: "Youtube", content: "Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh.")
-post6 = Post.create(username:"Kale",      subject: "Tech", content: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.")
-post7 = Post.create(username:"Arnuad",    subject: "Tech", content: "Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.")
-post8 = Post.create(username:"Eadie",     subject: "Sports", content: "Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.")
-post9 = Post.create(username:"Worthington",subject: "Sports", content: "Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius.")
-post10 =Post.create(username:"Vivie",     subject: "Food", content: "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis.")
+
+# hacks = Subject.create.(subject: "Hacks")
+# food = Subject.create.(subject: "Food")
+# digital_photo = Subject.create.(subject: "Digital Photography")
+# youtube = Subject.create.(subject: "Youtube")
+# tech = Subject.create.(subject: "Tech")
+# sports = Subject.create.(subject: "Sports")
+
+post1 = Post.create(username:"Stanislaus",subject: hacks, content: "Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.")
+post2 = Post.create(username:"Gav",       subject: food, content: "Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.")
+post3 = Post.create(username:"Obadias",   subject: digital_photo, content: "Suspendisse potenti. Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus.")
+post4 = Post.create(username:"Dulci",     subject: food, content: "Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus. Curabitur at ipsum ac tellus semper interdum.")
+post5 = Post.create(username:"Nanice",    subject: youtube, content: "Integer non velit. Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque. Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl. Duis ac nibh.")
+post6 = Post.create(username:"Kale",      subject: tech, content: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.")
+post7 = Post.create(username:"Arnuad",    subject: tech, content: "Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.")
+post8 = Post.create(username:"Eadie",     subject: sports, content: "Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.")
+post9 = Post.create(username:"Worthington",subject: sports, content: "Aliquam erat volutpat. In congue. Etiam justo. Etiam pretium iaculis justo. In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius.")
+post10 =Post.create(username:"Vivie",     subject: food, content: "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti. Nullam porttitor lacus at turpis.")
 
 
 post1.comments.create(username: "Arda",    text: "Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.")
 post1.comments.create(username: "Birch",   text: "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.")
 post2.comments.create(username: "Dani",    text: "In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.")
 post2.comments.create(username: "Marjory", text: "Nunc purus. Phasellus in felis. Donec semper sapien a libero.")
-post2.comments.create(username: "Arnaldo", text: "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.")
+post3.comments.create(username: "Arnaldo", text: "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.")
+post3.comments.create(username: "Arda",    text: "Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.")
+post4.comments.create(username: "Birch",   text: "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.")
+post4.comments.create(username: "Dani",    text: "In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.")
+post5.comments.create(username: "Marjory", text: "Nunc purus. Phasellus in felis. Donec semper sapien a libero.")
+post5.comments.create(username: "Arnaldo", text: "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.")
+post6.comments.create(username: "Arda",    text: "Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.")
+post6.comments.create(username: "Birch",   text: "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.")
+post7.comments.create(username: "Dani",    text: "In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.")
+post7.comments.create(username: "Marjory", text: "Nunc purus. Phasellus in felis. Donec semper sapien a libero.")
+post8.comments.create(username: "Arnaldo", text: "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.")
+post8.comments.create(username: "Birch",   text: "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.")
+post9.comments.create(username: "Dani",    text: "In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.")
+post9.comments.create(username: "Marjory", text: "Nunc purus. Phasellus in felis. Donec semper sapien a libero.")
+post10.comments.create(username: "Arnaldo", text: "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.")
+post10.comments.create(username: "Birch",   text: "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.")
+post10.comments.create(username: "Dani",    text: "In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.")
+post10.comments.create(username: "Marjory", text: "Nunc purus. Phasellus in felis. Donec semper sapien a libero.")
+post10.comments.create(username: "Arnaldo", text: "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.")
+
+
 
 
 
