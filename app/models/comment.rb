1308bda81c04075c83ba8a713abd83fc19fd570e
Fitssum Haile <fitsum.wolde@gmail.com>
2017-06-07 03:43:50 -0400

    added validates to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  validates :comment_name, :num_comment, :comment_url, {presence: true}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   has_many :comments
+  validates :post_name, :topic, :post_url, {presence: true}
 end
