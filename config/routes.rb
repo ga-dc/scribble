e0bbaee86f2330ddef224eb007e838d984c402b0
Tarik Kazanci <tarikkzc@gmail.com>
2017-06-07 09:13:30 -0400

    added user sign in/sign up and created association between user and post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/posts' => 'posts#index'
+
+root to: "posts#index"
+
+# devise_for :users, :path => 'accounts'
+
+devise_for :users
+
+  resources :posts do
+    resources :comments
+  end
 end
