8683abf35a971b487d41a4aa949d52c0344b7c28
Robert Lowe <RobLowe1992@Yahoo.com>
2017-06-07 08:11:57 -0400

    Moved folders to homework

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "posts#index"
+resources :users do
+    resources :posts
+end
+
+resources :posts do
+    resources :comments
+  end
+end
