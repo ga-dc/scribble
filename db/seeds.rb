7e5ceab1291913e5385c7b476b17380ea8ce8aec
Perry Fustero <pjfust@gmail.com>
2017-07-27 14:15:03 -0400

    models/ migration complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.destroy_all
+Post.destroy_all
+
+first_post = Post.create(title: "Breaking News!", body: "Today we learned about Rails!", photo_url: "www.google.com")
+
+second_post = Post.create(title: "Trip to Post", body: "Today we take a field trip to the post", photo_url: "www.washingtonpost.com")
+
+first_comment = Comment.create(title: "You suck!", body: "I want to learn about Python!", post: first_post)
+
+second_comment = Comment.create(title: "You are great!", body: "I love Rails!", post: first_post)
