77623000cd962967bd9e88ac19ec7e704add6ccc
Kendall <adkins.kendall90@gmail.com>
2017-07-31 19:13:57 -0400

    finish regular - start bonus

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,31 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(comments_params)
-    
+
+    redirect_to post_path(@post)
+  end
+
+  # def show
+  #   @comment = Comment.find(params[:id])
+  # end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update!(comments_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy!
+
     redirect_to post_path(@post)
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
+  has_many :tags, dependent: :destroy
 
   validates :title, :author, :content, { presence: true }
 end
