84c86a76e5e546c98cc28721d7ebb9a4071cbef3
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-01 14:17:00 -0400

    created comment controller logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,50 @@
 class CommentsController < ApplicationController
-  def index
+
+  # new
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+  end
+
+  # create
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(comment_params)
+    flash[:notice] = "Comment was created."
+    redirect_to post_path(@post)
+  end
+
+  #show
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  # edit
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  # update
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    flash[:notice] = "Comment was Updated."
+
+    redirect_to post_comment_path(@post, @comment)
+  end
+
+  # destroy
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@comment.post)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:author, :content)
   end
 end
