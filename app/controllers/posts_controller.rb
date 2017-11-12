0cd784a699577a75e8ad739fd19b2980a5d3c9e1
Sloan Holzman <sloan.holzman@gmail.com>
2017-11-12 14:13:51 -0500

    allow delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,13 @@ class PostsController < ApplicationController
     redirect_to "/posts/#{@post.id}"
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+
   private
   def post_params
     params.require(:post).permit(:title, :body, :author)
