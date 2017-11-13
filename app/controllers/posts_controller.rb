13519e6e3c2cb00afb6ecb4eba80f080bcd397d6
Stella <sgordonzigel@gmail.com>
2017-11-13 14:19:50 -0500

    change routes format and add create post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create!(post_params)
+
+    redirect_to post_path(@post)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:author, :title, :content)
   end
 end
