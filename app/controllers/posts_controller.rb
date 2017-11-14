ca199214f7b42869497cc6cb1d804232a5f8eafd
Stella <sgordonzigel@gmail.com>
2017-11-14 09:39:25 -0500

    create, update, delete functionality, add messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,29 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create!(post_params)
+    if @post.save
+      redirect_to post_path(@post), notice: "#{@post.title} was succesfully created."
+    else
+      render :new
+    end
+  end
 
-    redirect_to post_path(@post)
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    redirect_to post_path(@post), notice: "#{@post.title} was successfully updated."
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path, notice: "#{@post.title} was successfully deleted."
   end
 
   private
