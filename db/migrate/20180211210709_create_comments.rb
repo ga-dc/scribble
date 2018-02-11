e1b5c46d81188448956579ac35c31d816d9a6ea3
Annette Coll <annette.coll@outlook.com>
2018-02-11 16:17:37 -0500

    foreign key added to db migrate comments

--- db/migrate/20180211210709_create_comments.rb
+++ db/migrate/20180211210709_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.belongs_to :posts
+      t.belongs_to :posts, foreign_key: true
     end
   end
 end
