039b5d4d6c51e57e2ae78ae1bd758d4bfb3db114
Kathy145 <Kathy.Lambert@LotusDesignsAgency.com>
2018-02-12 20:54:48 -0500

    Completed migrations for Post and Comment

--- db/migrate/20180213012954_create_comments.rb
+++ db/migrate/20180213012954_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
+      t.string :author
+      t.string :text
+      t.references :post, foreign_key: true
     end
   end
 end
--- db/migrate/20180213012959_create_posts.rb
+++ db/migrate/20180213012959_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
+      t.string :title
+      t.string :author
+      t.string :text
     end
   end
 end
