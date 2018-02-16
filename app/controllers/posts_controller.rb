6d10f37bacdaf37e96a54a23810d0cc4449d45c9
K. Eaglin <kceaglin@gmail.com>
2018-02-15 23:59:41 -0500

    posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,41 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create!(post_params)
+    redirect_to "posts/#{@post.id}"
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/posts"
+  end
+
+  # strong params; used to sanitize form input
+  private
+    def post_params
+      params.required(:post).permit(:title, :author, :body) 
+    end
 end
+  
