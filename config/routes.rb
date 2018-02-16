a320b657188d3e1bd3651ec96d448e2539c2b227
Zelalem Tolera <tolerazelalem@gmail.com>
2018-02-15 23:19:00 -0500

    generating restful routes using resources keyword

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get "/posts"                => "posts#index"
-  get "/posts/new"            => "posts#new"
-  get "/posts/:id"            => "posts#show"
-  post "/posts"               => "posts#create"
+  root to: "posts#index"
+  resources :posts do
+    resources :comments
+  end
 end
